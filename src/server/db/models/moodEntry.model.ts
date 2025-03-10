e313de7e91271383169a482d294e3c0ab9ecc9d6
MrCrowNFT <mrcrownft@gmail.com>
2025-03-10 11:03:36 -0300

    moodEntry schema model separation

--- src/server/db/models/moodEntry.model.ts
+++ src/server/db/models/moodEntry.model.ts
@@ -1,16 +1,5 @@
 import mongoose from "mongoose";
-import { z } from "zod";
 
-//zod schema for validation
-export const MoodEntrySchema = z.object({
-  userId: z.string().min(1),
-  mood: z.number().int().min(1).max(5),
-  createdAt: z.date().optional(),
-  updated: z.date().optional(),
-});
-
-//type
-export type MoodEntry = z.infer<typeof MoodEntrySchema>;
 
 //mongoose schema
 const moodEntrySchema = new mongoose.Schema(
