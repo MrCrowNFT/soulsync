39094b04aa42de547ba84546c9351620c0314211
MrCrowNFT <mrcrownft@gmail.com>
2025-03-10 17:40:42 -0300

    Mood entry repository

---
+++ src/server/db/repositories/moodEntry.repository.ts
@@ -0,0 +1,34 @@
+import { MoodEntryModel } from "../models/moodEntry.model";
+import { MoodEntrySchema } from "../schemas/moodEntry.schema";
+import type { MoodEntry } from "../schemas/moodEntry.schema";
+import mongoose from "mongoose";
+
+export const MoodEntryRepository = {
+  create: async (data: MoodEntry) => {
+    const parsedData = MoodEntrySchema.parse(data); //validate
+    const newMoodEntry = await MoodEntryModel.create(parsedData);
+    return newMoodEntry;
+  },
+
+  gettById: async (id: string) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) throw new Error();
+    return await MoodEntryModel.findById(id).exec();
+  },
+  getAllByUserId: async (userId: string) => {
+    if (!mongoose.Types.ObjectId.isValid(userId))
+      throw new Error("Invalid User ID");
+    return await MoodEntryModel.find({ userId }).exec();
+  },
+  updateById: async (id: string, updateData: Partial<MoodEntry>) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) throw new Error("Invalid ID");
+    const parsedData = MoodEntrySchema.partial().parse(updateData); // Validate update fields
+    return await MoodEntryModel.findByIdAndUpdate(id, parsedData, {
+      new: true,
+      runValidators: true,
+    }).exec();
+  },
+  deleteById: async (id: string) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) throw new Error("Invalid ID");
+    return await MoodEntryModel.findByIdAndDelete(id).exec();
+  },
+};
