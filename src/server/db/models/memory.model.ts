2f7ae65c97d120bc195dc803fda001d6fdec2ee3
MrCrowNFT <mrcrownft@gmail.com>
2025-03-10 09:42:51 -0300

    Memory zod schema

---
+++ src/server/db/models/memory.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import {z} from "zod";
+
+//zod schema validation
+export const MemorySchema = z.object({
+    userId: z.string().min(1),
+    memory: z.string().min(1),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+})
+
+//type
+export type Memory = z.infer<typeof MemorySchema>;
