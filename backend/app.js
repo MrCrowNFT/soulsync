667e9c5d5305c2e9fd1e8e70dd6ccd361826a0f6
MrCrowNFT <mrcrownft@gmail.com>
2025-04-02 08:53:50 -0300

    App

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors()); //allow all origins for now
+
+const PORT = process.env.PORT || 5500;
+app.liste(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
