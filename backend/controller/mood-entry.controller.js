2a4489e14a597026d27b91f670506167ca89dc96
MrCrowNFT <mrcrownft@gmail.com>
2025-04-08 09:06:45 -0400

    mood entry controller

--- backend/controller/mood-entry.controller.js
+++ backend/controller/mood-entry.controller.js
@@ -1,5 +1,7 @@
+import req from "express/lib/request";
 import { MoodEntry } from "../modules/mood-entry.model";
 import mongoose from "mongoose";
+import { getMoodAverages } from "../helpers/mood-entry.helper";
 
 export const newMoodEntry = async (req, res) => {
   try {
@@ -46,3 +48,66 @@ export const newMoodEntry = async (req, res) => {
     });
   }
 };
+
+export const getEntries = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const type = req.body;
+
+    if (!userId || !type) {
+      res
+        .status(400)
+        .json({ success: false, error: "userId and type are required" });
+      return;
+    }
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "invalid user id" });
+    }
+    const averages = await getMoodAverages(userId, type);
+    return res.status(200).json({ success: true, data: averages });
+  } catch (error) {
+    console.error("Error in getEntries:", error);
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+      details: error.message,
+    });
+  }
+};
+
+//todo the only moment when i might need to delete the entries are in a bundle
+//todo when the user is deleting his account, thus, i will turn this one into a
+//todo helper function and add a delete all endpoint. maybe
+export const deleteMoodEntry = async (req, res) => {
+  try {
+    const { moodId } = req.body;
+
+    if (!moodId) {
+      res.status(400).json({ success: false, error: "moodId is required" });
+      return;
+    }
+
+    const deletedMoodEntry = await MoodEntry.findByIdAndDelete(moodId);
+
+    if (!deletedMoodEntry) {
+      res.status(404).json({ success: false, error: "Mood entry not found" });
+      return;
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, message: "Mood entry deleted successfully" });
+  } catch (error) {
+    console.error("Error in deleteMoodEntry:", error);
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+      details: error.message,
+    });
+  }
+};
+
+//todo the system is design to simply add and get, there's still no need for the
+//todo update function, still need t
