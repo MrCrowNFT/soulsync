d4931e67cb1c1532ead0e428365f9b8cdb73dc64
MrCrowNFT <mrcrownft@gmail.com>
2025-04-10 09:45:51 -0400

    user types

--- frontend/src/api/services/user.ts
+++ frontend/src/api/services/user.ts
@@ -1,28 +1,9 @@
-import { IUser } from "../../types/user";
+import {
+  deleteAccountResponse,
+  updateUserPayload,
+  updateUserResponse,
+} from "../../types/user";
 import api from "../axios";
-
-export interface updateUserPayload {
-  username?: string;
-  password?: string;
-  email?: string;
-  name?: string;
-  lastName?: string;
-  gender?: "male" | "female" | "non-binary" | "other" | "prefer-not-to-say";
-  birthDate?: Date;
-  photo?: string;
-}
-
-export interface updateUserResponse {
-  success: boolean;
-  message: string;
-  user: IUser;
-}
-//i know i can just extend this response onto the update one
-//but i could not come up with a good name
-export interface deleteAccountResponse {
-  success: boolean;
-  message: string;
-}
 
 export const updateUser = async (
   updateUserPayload: updateUserPayload
--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -12,3 +12,26 @@ export interface IUser {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface updateUserPayload {
+  username?: string;
+  password?: string;
+  email?: string;
+  name?: string;
+  lastName?: string;
+  gender?: "male" | "female" | "non-binary" | "other" | "prefer-not-to-say";
+  birthDate?: Date;
+  photo?: string;
+}
+
+export interface updateUserResponse {
+  success: boolean;
+  message: string;
+  user: IUser;
+}
+//i know i can just extend this response onto the update one
+//but i could not come up with a good name
+export interface deleteAccountResponse {
+  success: boolean;
+  message: string;
+}
