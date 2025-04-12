53be36b997196e0abfc72a2ce107757a0951af80
MrCrowNFT <mrcrownft@gmail.com>
2025-04-12 16:35:43 -0400

    auth routes app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
 import Chat from "./components/chat";
+import { LoginForm } from "./components/login";
+import { SignupForm } from "./components/signup";
 
 //todo ROUTES
 function App() {
@@ -11,9 +13,11 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/chat" element={<Chat/>}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
       </Routes>
       <Footer />
     </>
