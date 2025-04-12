cf554231aa1686abc372909b14305b0b190d1461
MrCrowNFT <mrcrownft@gmail.com>
2025-04-12 16:31:34 -0400

    dashboard

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,1 +1,14 @@
-//todo
+import MoodGraph from "@/components/mood-graph"
+import UserCard from "@/components/user-card"
+
+
+const Dashboard =()=>{
+    return (
+        <>
+        <UserCard/>
+        <MoodGraph/>
+        </>
+    )
+}
+
+export default Dashboard
