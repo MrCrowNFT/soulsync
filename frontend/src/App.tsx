5e2403f42fcf1d326b748466d93964b01baab6a3
MrCrowNFT <mrcrownft@gmail.com>
2025-04-12 16:33:36 -0400

    App routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import Chat from "./components/chat";
 
 //todo ROUTES
 function App() {
@@ -8,7 +11,9 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/home" element={}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/chat" element={<Chat/>}/>
       </Routes>
       <Footer />
     </>
