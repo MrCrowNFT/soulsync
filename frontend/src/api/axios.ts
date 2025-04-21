5754997e25f0e9f5d5648efa46be69ef7e98581f
MrCrowNFT <mrcrownft@gmail.com>
2025-04-21 11:38:19 -0400

    set backend api in .env

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,8 +2,10 @@ import axios, { AxiosInstance } from "axios";
 import { setupRequestInterceptor } from "./interceptors/request";
 import { setupResponseInterceptor } from "./interceptors/response";
 
+const urlApi = import.meta.env.API_URL;
+
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5500",
+  baseURL: urlApi,
   withCredentials: true,
 });
 setupRequestInterceptor(api);
