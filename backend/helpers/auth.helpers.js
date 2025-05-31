a1957cb46ba3d86b3b2d6827e66bf8506879809f
MrCrowNFT <mrcrownft@gmail.com>
2025-05-30 21:10:10 -0400

    Auth helper logs

--- backend/helpers/auth.helpers.js
+++ backend/helpers/auth.helpers.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import logger from "./logger.js";
+import logger from "../utils/logger.js";
 
 // Constants for token expiration -> subject to change
 const ACCESS_TOKEN_EXPIRY = "5m";
