e6e232a6fdb39e83eab376295e5d5c00ae48deb5
MrCrowNFT <mrcrownft@gmail.com>
2025-06-05 10:10:58 -0400

    multer added to update user route

--- backend/routers/user.routes.js
+++ backend/routers/user.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { deleteUserAccount, updateUser } from "../controller/user.controller.js";
+import {
+  deleteUserAccount,
+  updateUser,
+} from "../controller/user.controller.js";
+import multer from "multer";
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  // no limits or fileFilter since the uploader handles validation
+});
 
 const userRouter = express.Router();
 
-userRouter.put("/", updateUser);
+userRouter.put("/", upload.single("photo"), updateUser);
 userRouter.delete("/", deleteUserAccount);
 
 export default userRouter;
